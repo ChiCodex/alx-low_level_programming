e8b095ddc52d94f1143941e36225f4c2498452e5
ChiCodex <kaylafrancisca420@gmail.com>
2023-08-10 16:12:42 -0700

    betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-pArintf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
