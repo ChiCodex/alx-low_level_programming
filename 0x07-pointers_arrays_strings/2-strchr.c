9121775680d87e9030b3bce4a47618d7470637fd
ChiCodex <kaylafrancisca420@gmail.com>
2023-08-28 16:23:07 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - locates a string in a character
+ * @s: first pointer
+ * @c: second parameter
+ *  Return: Always 0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+
+
